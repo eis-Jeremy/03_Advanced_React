1d15bdc1be8cc216dcd421704ef9ec82933afac2
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 13:21:42 +0200

    Test Rückgängig

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,46 +1,43 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h1>Test</h1>
+  const [users, setUsers] = useState([]);
 
-//   const [users, setUsers] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const users = await response.json();
+        setUsers(users)
+      } catch (error) {
+        console.log(error);
+      }
 
+    };
+    fetchData();
+  }, [])
 
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const response = await fetch(url);
-//         const users = await response.json();
-//         setUsers(users)
-//       } catch (error) {
-//         console.log(error);
-//       }
-
-//     };
-//     fetchData();
-//   }, [])
-
-//   return (
-//     <section>
-//       <h3>GitHub users</h3>
-//       <ul className="users">
-//         {users.map((user) => {
-//           const {id, login, avatar_url, html_url} = user;
-//           return (
-//             <li key={id}>
-//               <img src={avatar_url} alt={login}/>
-//               <div>
-//                 <h5>{login}</h5>
-//                 <a href={html_url}>Profile</a>
-//               </div>
-//             </li>
-//           )
-//         })}
-//       </ul>
-//     </section>
-//   );
+  return (
+    <section>
+      <h3>GitHub users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const {id, login, avatar_url, html_url} = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login}/>
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </section>
+  );
 };
 export default FetchData;
 
