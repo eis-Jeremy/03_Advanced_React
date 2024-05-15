3e2053563d7b0f0d49626de1d7bf3b0ea8b72c7f
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 15:54:06 +0200

    VorschauLuca2

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
-  return <>
-    <h2>Hallo Luca!</h2>
-    <p>Diese Webseite ist zwar nicht viel, soll sie aber auch nicht sein.</p>
-    <hr />
-    <p>Sie dient einzig und allein der Zeitverschwendung! ^^\nIch hoffe dir gefällt's! ^^'</p>
-  </>
+  return (
+    <>
+      <h2>Hallo Luca!</h2>
+      <p>Diese Webseite ist zwar nicht viel, soll sie aber auch nicht sein.</p>
+      <hr />
+      <p>Sie dient einzig und allein der Zeitverschwendung! ^^'</p>
+      <p>Ich hoffe dir gefällt's! ^^</p>
+    </>
+  )
 };
 export default MultipleReturnsBasics;
