cf21d231fcbe7d176970693d61f138226dc530bc
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 16:11:03 +0200

    Multiple returns v2

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
+  
   function sayHello(name) {
     if (name) {
       return <h2>Hello {name}!</h2>
@@ -9,6 +10,6 @@ const MultipleReturnsBasics = () => {
     return <h2>Hello there</h2>
   }
   
-  sayHello('Jeremy')
+  return sayHello('Jeremy')
 };
 export default MultipleReturnsBasics;
