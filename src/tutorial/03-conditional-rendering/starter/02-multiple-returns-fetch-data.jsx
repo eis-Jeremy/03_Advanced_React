17e4aa64a51c37008b60f24835a4d4b7bcd316fe
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 17:41:33 +0200

    Multiple returns -- Fetch data example - v9

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,39 +1,47 @@
 import { useEffect, useState } from 'react'
-const url = 'https://api.github.com/users/QuincyLarson'
+const url = 'https://api.github.com/users/QuincyLarsons'
 
 const MultipleReturnsFetchData = () => {
-  
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true)
   const [isError, setIsError] = useState(false)
-  const [user, setUser]  =useState(null)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     async function fetchUser() {
       try {
         const resp = await fetch(url)
-        const user = await resp.json();
+        if (!resp.ok) {
+          setIsError(true)
+          setIsLoading(false)
+        }
+        const user = await resp.json()
         setUser(user)
       } catch (error) {
-        setIsError(true);
+        setIsError(true)
       }
       setIsLoading(false)
     }
-    
+
     fetchUser()
   }, [])
-
 
   if (isLoading) {
     return <h2>Loading...</h2>
   } else if (isError) {
     return <h2>There was an error! Please try again.</h2>
   }
-  
-  return <>
-    <img src={user.avatar_url} alt={user.name} style={{width: '150px', borderRadius: '25px'}} />
-    <h2>{user.name}</h2>
-    <h4>Works at: {user.company}</h4>
-    <p>{user.bio}</p>
-  </>
+
+  return (
+    <>
+      <img
+        src={user.avatar_url}
+        alt={user.name}
+        style={{ width: '150px', borderRadius: '25px' }}
+      />
+      <h2>{user.name}</h2>
+      <h4>Works at: {user.company}</h4>
+      <p>{user.bio}</p>
+    </>
+  )
 }
 export default MultipleReturnsFetchData
