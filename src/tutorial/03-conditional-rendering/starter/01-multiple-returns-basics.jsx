9b2ca4df68cb47059aabd4afbfe94966fd446d7e
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 15:29:13 +0200

    Vorschau für Antony

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
+  return <>
+    <h2>Hallo Antony!</h2>;
+    <p>Diese Webseite wird auf der PS5 angezeigt! (Sieht man ja😅)</p>
+  </>
 };
 export default MultipleReturnsBasics;
