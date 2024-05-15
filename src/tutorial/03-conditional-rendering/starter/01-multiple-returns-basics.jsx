737a82e32552473b24c9b88e2a8d91a1e64080f0
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 15:48:07 +0200

    Vorschau2

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
   return <>
-    <h2>Hallo Antony!</h2>;
+    <h2>Hallo Antony!</h2>
     <p>Diese Webseite wird auf der PS5 angezeigt! (Sieht man ja😅)</p>
   </>
 };
