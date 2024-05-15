496e4355cc68eb69026c2683221b1e71ccf2e40f
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 13:28:54 +0200

    Begin conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,14 @@ import UseEffect03 from './tutorial/02-useEffect/starter/03-multiple-effects'
 import UseEffect04 from './tutorial/02-useEffect/starter/04-fetch-data'
 import UseEffect05 from './tutorial/02-useEffect/starter/05-cleanup-function'
 
+import ConditionalRendering01 from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics'
+import ConditionalRendering02 from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data'
+import ConditionalRendering03 from './tutorial/03-conditional-rendering/starter/03-hooks-rule'
+import ConditionalRendering04 from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview'
+import ConditionalRendering05 from './tutorial/03-conditional-rendering/starter/05-short-circuit-examples'
+import ConditionalRendering06 from './tutorial/03-conditional-rendering/starter/06-toggle-challenge'
+import ConditionalRendering07 from './tutorial/03-conditional-rendering/starter/07-user-challenge'
+
 function App() {
   return (
     <div className="container">
@@ -18,21 +26,38 @@ function App() {
       {/* <UseState02 /> */}
 
       {/* <UseState03 /> */}
-      
+
       {/* <UseState04 /> */}
-      
+
       {/* <UseState05 /> */}
-      
+
       {/* <UseEffect01 /> */}
-      
+
       {/* <UseEffect02 /> */}
-      
+
       {/* <UseEffect03 /> */}
-      
-      <UseEffect04 />
-      
+
+      {/* <UseEffect04 /> */}
+
+      {
+        //NACH CONDITIONAL RENDERING
+      }
       {/* <UseEffect05 /> */}
-     
+
+      <ConditionalRendering01 />
+
+      {/* <ConditionalRendering02 /> */}
+
+      {/* <ConditionalRendering03 /> */}
+
+      {/* <ConditionalRendering04 /> */}
+
+      {/* <ConditionalRendering05 /> */}
+
+      {/* <ConditionalRendering06 /> */}
+
+      {/* <ConditionalRendering07 /> */}
+
     </div>
   )
 }
