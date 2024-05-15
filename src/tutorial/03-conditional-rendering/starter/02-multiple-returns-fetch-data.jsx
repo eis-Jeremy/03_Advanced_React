91c5b0449e523fb7fa4ea04734d0da400ae57dac
eis-Jeremy <eis08.jerry@gmail.com>
2024-05-15 17:07:15 +0200

    Multiple returns -- Fetch data example - v4

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState(null)
 
   const test = useEffect(() => {
-    async function fetchUser () {
+    async function fetchUser() {
       try {
-        const resp = await fetch(url);
-        const user = await resp.json();
+        const resp = await fetch(url)
+        const user = await resp.json()
         return <h2>{user}</h2>
       } catch (error) {
         return <h2>{error}</h2>
       }
     }
 
-
     return fetchUser()
   }, [])
 
-  return test()
-};
-export default MultipleReturnsFetchData;
+  return test
+}
+export default MultipleReturnsFetchData
